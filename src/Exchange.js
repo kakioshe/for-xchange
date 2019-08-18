55034457f1f3ded1fe8d181fef7c29542a802fce
kakioshe <louischristoph.alan@gmail.com>
2019-08-19 00:33:56 +0700

    added remove currency function, basic css editing

--- src/Exchange.js
+++ src/Exchange.js
@@ -2,39 +2,44 @@ import React, { Component } from "react";
 import ExchangeItems from "./ExchangeItems";
 import Select from 'react-select';
 
+import { Grid, Container, Row, Col, Button } from 'react-bootstrap';
+
 const currencyOptions = [
-  { value: 'USD', label: 'USD'},
-  { value: 'CAD', label: 'CAD'},
-  { value: 'IDR', label: 'IDR'},
-  { value: 'GBP', label: 'GBP'},
-  { value: 'CHF', label: 'CHF'},
-  { value: 'SGD', label: 'SGD'},
-  { value: 'INR', label: 'INR'},
-  { value: 'MYR', label: 'MYR'},
-  { value: 'JPY', label: 'JPY'},
-  { value: 'KRW', label: 'KRW'},
+  { value: 'USD', label: 'USD', text:'United States Dollar'},
+  { value: 'CAD', label: 'CAD', text:'Canadian Dollar'},
+  { value: 'IDR', label: 'IDR', text:'Indonesian Rupiah'},
+  { value: 'GBP', label: 'GBP', text:'Pound Sterling'},
+  { value: 'CHF', label: 'CHF', text:'Swiss Franc'},
+  { value: 'SGD', label: 'SGD', text:'Singapore Dollar'},
+  { value: 'INR', label: 'INR', text:'Indian Rupee'},
+  { value: 'MYR', label: 'MYR', text:'Malaysian Ringgit'},
+  { value: 'JPY', label: 'JPY', text:'Japanese Yen'},
+  { value: 'KRW', label: 'KRW', text:'South Korean Won'},
 ]
 class Exchange extends Component {
   constructor(props) {
     super(props);
     this.addItem = this.addItem.bind(this);
     this.updateBaseCurrency = this.updateBaseCurrency.bind(this);
+    this.handleChange = this.handleChange.bind(this);
 
     this.state = {
       items: [],
       data: [],
       baseCur: {
         value: "USD",
-        label: "USD"
+        label: "USD",
+        text: "United States Dollar"
       },
+      value: 10.00,
       selectedCur: null,
     };
   }
   addItem(e){
     if (this.state.selectedCur !== "") {
       var newItem = {
-        text: this.state.selectedCur.value,
-        key: Date.now()
+        key: this.state.selectedCur.value,
+        text: this.state.selectedCur.text
       };
 
       this.setState((prevState) => {
@@ -68,36 +73,63 @@ class Exchange extends Component {
     this.fetchAPI(baseCur.value);
   }
 
-  handleChange = selectedCur => {
+  addCur = selectedCur => {
     this.setState({ selectedCur })
-    console.log(selectedCur);
   }
+
+  deleteCurrency = currencyKey => {
+    const items = this.state.items.filter(item => item.key !== currencyKey);
+    this.setState({ items: items});
+  }
+
+  handleChange (event) {
+    this.setState({ value: event.target.value })
+  }
+
   componentDidMount(){
     this.fetchAPI();
   }
   render() {
     return (
-      <div className="exchangeMain">
-        <div className="header">
-          <Select
-            value={this.state.baseCur}
-            onChange={this.updateBaseCurrency}
-            options={currencyOptions}/>
-        </div>
-        <br />
-        <div className="body">
-          <form onSubmit={this.addItem}>
-            <ExchangeItems entries={this.state.items} rates={this.state.rates}/>
-            <Select value = {this.state.selectedCur}
-                    placeholder="Item Name"
-                    onChange={this.handleChange}
+      <Container>
+        <Row>
+          <div className="exchangeMain">
+            <div className="header">
+              <p>{this.state.baseCur.value} - {this.state.baseCur.text}</p>
+              <Row>
+                <Col sm={8}>
+                  <Select
+                    value={this.state.baseCur}
+                    onChange={this.updateBaseCurrency}
                     options={currencyOptions}/>
-            <button type="submit">Add</button>
-          </form>
-        </div>
-        <p>{this.state.data.base}</p>
-        <p>{this.state.baseCur.value}</p>
-      </div>
+                </Col>
+                <Col sm={4}>
+                  <input
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.handleChange} />
+                </Col>
+              </Row>
+            </div>
+            <br />
+            <div className="body">
+              <form onSubmit={this.addItem}>
+                <ExchangeItems
+                  entries={this.state.items}
+                  rates={this.state.rates}
+                  value={this.state.value}
+                  currentCur={this.state.baseCur.value}
+                  onDelete={this.deleteCurrency}/>
+                <Select value = {this.state.selectedCur}
+                        placeholder="Item Name"
+                        onChange={this.addCur}
+                        options={currencyOptions}/>
+                <Button variant="primary" type="submit">Add</Button>
+              </form>
+            </div>
+          </div>
+        </Row>
+    </Container>
     )
   }
 }
--- src/ExchangeItems.js
+++ src/ExchangeItems.js
@@ -1,20 +1,45 @@
 import React, { Component } from "react";
+import { Grid, Container, Row, Col, Button } from 'react-bootstrap';
 
 class ExchangeItems extends Component {
-  
+
   render(){
     var currencyEntries = this.props.entries;
     var currencyRates = this.props.rates;
+    var value = this.props.value;
+    var currentCur = this.props.currentCur;
 
     var listItems = currencyEntries.map((item, i) =>{
-      const tempRate = `this.props.rates.${item.text}`;
-      return <li key={i}> {item.text} { currencyRates[item.text] }</li>
+      const convertedAmount = (value * currencyRates[item.key]).toFixed(2);
+      console.log(value);
+      return (
+        <Row key={i}>
+          <Col>
+            <div>
+              <Row>
+                <Col>{item.key}</Col>
+                <Col>{convertedAmount}</Col>
+              </Row>
+              <Row>
+                <Col><p>{item.key} - {item.text}</p></Col>
+              </Row>
+              <Row>
+                <Col><p>1 {currentCur} = {item.key} {currencyRates[item.key].toFixed(4)} </p></Col>
+              </Row>
+            </div>
+          </Col>
+          <Col>
+            <Button onClick={() => this.props.onDelete(item.key)} variant="danger">Delete</Button>
+          </Col>
+        </Row>
+      )
     });
+    console.log(listItems);
 
     return (
-      <ul className="listItem">
+      <Container>
         {listItems}
-      </ul>
+      </Container>
     )
   }
 }
